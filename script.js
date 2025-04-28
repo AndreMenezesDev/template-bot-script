695db7e698809d8ea6f55c8523d5db46933dea90
André Menezes <andreeliel@hotmail.com>
2025-04-27 23:38:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,8 @@ function criarBarra(var_nome, var_avatar) {
   const status = varStatus;
   
   const audioNot = document.createElement('audio');
-  audioNot.src = 'https://github.com/igorlemoes/template_typebot_whatsapp/raw/main/audio_whatsapp.mp3';
+  //audioNot.src = 'https://github.com/igorlemoes/template_typebot_whatsapp/raw/main/audio_whatsapp.mp3';
+  audioNot.src = 'https://github.com/AndreMenezesDev/template-bot-script/blob/main/audio_whatsapp.mp3';
   let mensagesLength = 0;
   
   setInterval(() => {
